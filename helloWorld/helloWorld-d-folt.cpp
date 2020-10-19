08c845c79b1eb5e1f7a86a5f7cafe6bb6e96d651
Uvindu Harshana <64182114+d-folt@users.noreply.github.com>
2020-10-18 20:02:54 -0700

    Create helloWorld-d-folt.cpp

---
+++ helloWorld/helloWorld-d-folt.cpp
@@ -0,0 +1,12 @@
+//header file for input and output
+#include <iostream>
+using namespace std;
+
+//main function
+int main() {
+
+  //print hello world
+  cout<<"Hello, world!";
+  
+  return 0;
+}
